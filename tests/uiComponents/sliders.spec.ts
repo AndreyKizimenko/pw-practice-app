e1e6fdb411536c80dbe37d94c6193f6a1cd93fa0
AndreyKizimenko <andreykizimenko@gmail.com>
2025-04-01 08:58:33 -0500

    sliders TCs

--- tests/uiComponents/sliders.spec.ts
+++ tests/uiComponents/sliders.spec.ts
@@ -1,7 +1,33 @@
 import { test, expect } from "@playwright/test";
 
-test.beforeEach(async ({ page }) => {
+test("sliders coordinates", async ({ page }) => {
   await page.goto("http://localhost:4200/");
+  const tempGauge = page.locator(
+    "[tabtitle='Temperature'] ngx-temperature-dragger circle"
+  );
+  await tempGauge.evaluate((node) => {
+    node.setAttribute("cx", "232.630");
+    node.setAttribute("cy", "232.630");
+  });
+  await tempGauge.click();
 });
 
-test.describe("sliders", () => {});
+test("sliders dragging", async ({ page }) => {
+  await page.goto("http://localhost:4200/");
+  const tempBox = page.locator(
+    "[tabtitle='Temperature'] ngx-temperature-dragger"
+  );
+  await tempBox.scrollIntoViewIfNeeded();
+
+  const box = await tempBox.boundingBox();
+  const x = box.x + box.width / 2;
+  const y = box.y + box.height / 2;
+
+  await page.mouse.move(x, y);
+  await page.mouse.down();
+  await page.mouse.move(x + 100, y);
+  await page.mouse.down();
+  await page.mouse.move(x + 100, y + 100);
+  await page.mouse.up();
+  await expect(tempBox).toContainText("30");
+});
